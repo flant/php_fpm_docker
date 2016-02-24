60820e8940f5335471fffa37577a076a158db948
Artemy Sergeev <artemy.sergeev@flant.ru>
2016-02-24 16:33:43 +0300

    * List for push to package_cloud via find

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-TARGET=$1
-
-make "$TARGET"
+for i in `find packages/ubuntu/12.04 -name '*.deb'`; do
+    package_cloud push flant/php_fpm_docker/ubuntu/precise $i
+done
+for i in `find packages/ubuntu/14.04 -name '*.deb'`; do
+    package_cloud push flant/php_fpm_docker/ubuntu/trusty $i
+done
